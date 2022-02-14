93f854dd6e561f61539dae3e009bda5a47dbf1b2
Roman Gofer <gofer.2@wright.edu>
2022-02-14 11:29:59 -0500

    Adding file to catch up repo

--- Projects/Discord-Bot/bot.py
+++ Projects/Discord-Bot/bot.py
@@ -1,28 +1,30 @@
 import os
+import random
 import re
 
 import discord
-import random
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
-client = discord.Client()
+client = discord.Client() 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+
+    print(f"{client.user} has connected to Discord!")
 
     for guild in client.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f"{client.user} is connected to the following guild:\n"
+        f"{guild.name}(id: {guild.id})"
     )
+
 
 @client.event
 async def on_message(message):
@@ -30,17 +32,30 @@ async def on_message(message):
         return
 
     betty_white_quotes = [
-        'Kindness and consideration of somebody besides yourself keeps you feeling young.',
-        'I have no regrets at all. None. I consider myself to be the luckiest old broad on two feet.',
-        'Everybody needs a passion. That’s what keeps life interesting. If you live without passion, you can go through life without leaving any footprints.',
-        'About being called a “legend:” “I just laugh. Have I got them fooled.” —On a CNN interview with Joy Behar',
-        'Retirement is not in my vocabulary. They aren’t going to get rid of me that way.',
-        'I’m a health nut. My favorite food is hot dogs with French fries. And my exercise: I have a two-story house and a very bad memory, so I’m up and down those stairs.'          
+        "Kindness and consideration of somebody besides yourself keeps you feeling young.",
+        "I have no regrets at all. None. I consider myself to be the luckiest old broad on two feet.",
+        "Everybody needs a passion. That’s what keeps life interesting. If you live without passion, you can go through life without leaving any footprints.",
+        "About being called a “legend:” “I just laugh. Have I got them fooled.” —On a CNN interview with Joy Behar",
+        "Retirement is not in my vocabulary. They aren’t going to get rid of me that way.",
+        "I’m a health nut. My favorite food is hot dogs with French fries. And my exercise: I have a two-story house and a very bad memory, so I’m up and down those stairs.",
     ]
-    pattern = '(?i)[^.]*(Betty White)[^.]*(.)?'
+    pattern = "(?i)[^.]*(Betty White)[^.]*(.)?"
     result = re.match(pattern, message.content)
     if result:
         response = random.choice(betty_white_quotes)
-        await message.channel.send(response, file=discord.File(random.choice(('images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg'))))
+        await message.channel.send(
+            response,
+            file=discord.File(
+                random.choice(
+                    (
+                        "images/img1.jpg",
+                        "images/img2.jpg",
+                        "images/img3.jpg",
+                        "images/img4.jpg",
+                    )
+                )
+            ),
+        )
+
 
 client.run(TOKEN)
